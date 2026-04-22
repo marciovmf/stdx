fbf0acaa68e7df7fe751b3b1976a99166c75efd4
marciovmmf <marciovmf@gmail.com>
2026-04-22 12:47:29 +0200

    include <stdlib> if DEBUG is defined so X_ASSERT macro can call abort()

--- src/stdx_common.h
+++ src/stdx_common.h
@@ -16,9 +16,9 @@
 
 #ifdef __cplusplus
 extern "C" {
-#include <stdbool.h>
 #endif
 
+#include <stdbool.h>
 
 #define X_COMMON_VERSION_MAJOR 1
 #define X_COMMON_VERSION_MINOR 0
@@ -52,7 +52,7 @@ extern "C" {
   //  Compiler ATTRIBUTES / INLINE / ALIGN
   // -----------------------------------------------------------------------------
 
-#if defined(COMPILER_MSVC)
+#if defined(X_COMPILER_MSVC)
 #define X_INLINE      __inline
 #define X_FORCEINLINE __forceinline
 #define X_NOINLINE    __declspec(noinline)
@@ -135,27 +135,6 @@ extern "C" {
 #ifndef DEBUG
 #define DEBUG
 #endif
-#endif
-
-
-  // ----------------------------------------------------------------------------
-  // Assertion macros
-  // ----------------------------------------------------------------------------
-
-#define X_STATIC_ASSERT(cond, msg) typedef char static_assert_##msg[(cond) ? 1 : -1]
-
-
-
-#ifdef DEBUG
-#define X_ASSERT(expr) \
-  do { \
-    if (!(expr)) { \
-      fprintf(stderr, "Assertion failed: %s, file %s, line %d\n", #expr, __FILE__, __LINE__); \
-      abort(); \
-    } \
-  } while(0)
-#else
-#define X_ASSERT(expr) ((void)0)
 #endif
 
   // ----------------------------------------------------------------------------
@@ -262,6 +241,26 @@ extern "C" {
 
 #ifndef X_ARRAY_COUNT
 #define X_ARRAY_COUNT(a) (sizeof(a)/sizeof((a)[0]))
+#endif
+
+
+  // ----------------------------------------------------------------------------
+  // Assertion macros
+  // ----------------------------------------------------------------------------
+
+#define X_STATIC_ASSERT(cond, msg) typedef char static_assert_##msg[(cond) ? 1 : -1]
+
+#ifdef DEBUG
+#include <stdlib.h>
+#define X_ASSERT(expr) \
+  do { \
+    if (!(expr)) { \
+      fprintf(stderr, "Assertion failed: %s, file %s, line %d\n", #expr, __FILE__, __LINE__); \
+      abort(); \
+    } \
+  } while(0)
+#else
+#define X_ASSERT(expr) ((void)0)
 #endif
 
 #ifdef __cplusplus
